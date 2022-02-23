c8248cd6e31388e00d06046e7144786cea499789
Pedro  C. Correa <98551189+pedroccorrea@users.noreply.github.com>
2022-02-23 12:09:21 -0300

    update

---
+++ unidade07/numeros3.php
@@ -0,0 +1,30 @@
+<?php
+    $gasolina = 4.55;
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+    </head>
+
+    <body>
+        
+        
+        <?php
+            // arredondar para media
+
+
+            // arredondar para cima
+
+
+            // arredondar para baixo
+
+
+            
+        ?>
+        
+        
+    </body>
+</html>
